c3e1480a9abfa1a5f24e07eab9f59629a055a91c
Nuria Rodriguez <nurialuzr1@gmail.com>
2020-09-13 03:11:52 -0400

    app connected to server

---
+++ server/app.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const express = require('express');
+const morgan = require("morgan");
+const app = express();
+
+// logging and body-parsing
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// You'll of course want static middleware so your browser can request things
+// like your 'index.html' and 'bundle.js'.
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+  });
+
+  app.use(function(err, req, res, next) {
+    console.error(err, err.stack);
+    res.status(err.status || 500);
+    res.send("Something went wrong: " + err.message);
+  });
+  
+  // listen on a port
+  const PORT = 3000;
+  
+  const init = async function() {
+    //await db.sync() // uncomment once you've done the db steps
+    app.listen(PORT, function() {
+      console.log(`Server is listening on port ${PORT}!`);
+    });
+  }
+  
+  init();
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,14 @@
+const path = require('path')
+
 module.exports = {
     mode: "development",
-    entry: './src/index.js', // the starting point for our program
+    entry: './client/index.js', // the starting point for our program
     output: {
-      path: __dirname + '/public', // the absolute path for the directory where we want the output to be placed
+      path: path.join(__dirname + '/public'), // the absolute path for the directory where we want the output to be placed
       filename: 'bundle.js' // the name of the file that will contain our output - we could name this whatever we want, but bundle.js is typical
-    }
+    },
+    devtool: 'source-map'
+      // Creates "source map" files (ex. "bundle.js.map"). Modern browsers can automatically
+  // request these to "rebuild" your original source code in your dev tools (i.e. the Sources tab).
+  // This makes debugging much, much nicer
   }
